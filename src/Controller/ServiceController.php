6e1ab7dcb2bbea411dbb601486f950fa63288ca9
Olha Sereda <olchik.sereda@gmail.com>
2024-05-06 21:42:21 +0200

    Add service route

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use App\Entity\Server;
 use App\Entity\Service;
 use App\Repository\ServiceRepository;
+use App\Repository\ServerRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ServiceController extends AbstractController
 {
@@ -30,25 +33,32 @@ class ServiceController extends AbstractController
         return new JsonResponse($data, Response::HTTP_OK);
     }
 
-
-    #[Route('/api/service', name: 'service_add', methods: ['POST'])]
-    public function addService(Request $request, EntityManagerInterface $em): JsonResponse
+    #[Route('/api/server/', name: 'service_add', methods: ['POST'])]
+    public function addService(Request $request, EntityManagerInterface $em, ServerRepository $serverRepository): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
     
         $service_name = $data['service_name'];
+        $server_id = $request->query->get('id');
     
         if (empty($service_name)) {
             return new JsonResponse(['status' => 'Service name is required'], Response::HTTP_BAD_REQUEST);
         }
     
+        $server = $serverRepository->find($server_id);
+        if (!$server) {
+            return new JsonResponse(['status' => 'Server not found'], Response::HTTP_NOT_FOUND);
+        }
+    
         $service = new Service();
         $service->setServiceName($service_name);
+        $service->setServer($server);
     
         $em->persist($service);
         $em->flush();
     
         return new JsonResponse(['status' => 'Service added successfully'], Response::HTTP_CREATED);
     }
+    
     
 }
